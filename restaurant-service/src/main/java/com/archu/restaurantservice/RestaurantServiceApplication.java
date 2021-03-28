e7fbc90f96442fb2c0cb79faa3b165108aaab079
MarcinPerka <marcinp1596@gmail.com>
2021-03-28 14:52:44 +0200

    Swagger

--- restaurant-service/src/main/java/com/archu/restaurantservice/RestaurantServiceApplication.java
+++ restaurant-service/src/main/java/com/archu/restaurantservice/RestaurantServiceApplication.java
@@ -1,5 +1,7 @@
 package com.archu.restaurantservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -10,6 +12,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableDiscoveryClient
 @EnableMongoAuditing
 @EnableMongoRepositories
+@OpenAPIDefinition(info = @Info(title = "Restaurant API", description = "Documentation Restaurant API"))
 public class RestaurantServiceApplication {
 
     public static void main(String[] args) {
--- review-service/src/main/java/com/archu/reviewservice/ReviewServiceApplication.java
+++ review-service/src/main/java/com/archu/reviewservice/ReviewServiceApplication.java
@@ -1,5 +1,7 @@
 package com.archu.reviewservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -10,6 +12,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableDiscoveryClient
 @EnableMongoAuditing
 @EnableMongoRepositories
+@OpenAPIDefinition(info = @Info(title = "Review API", description = "Documentation Review API"))
 public class ReviewServiceApplication {
 
     public static void main(String[] args) {

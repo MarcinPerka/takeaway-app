171a243ba38b26c26ddd47fbca7fd374e3fa3756
MarcinPerka <marcinp1596@gmail.com>
2021-04-18 11:35:06 +0200

    Base predicate

---
+++ takeaway-common-spring/src/main/java/com/archu/takeawaycommonspring/base/filter/BasePredicate.java
@@ -0,0 +1,90 @@
+package com.archu.takeawaycommonspring.base.filter;
+
+import com.querydsl.core.types.dsl.*;
+import lombok.RequiredArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@RequiredArgsConstructor
+public abstract class BasePredicate<T> {
+
+    private final SearchCriteria searchCriteria;
+
+    abstract BooleanExpression getPredicate(final Class<T> tClass, final String collectionName);
+
+    protected BooleanExpression getStringPredicate(final StringPath stringPath, final String value) {
+        switch (searchCriteria.getSearchOperation()) {
+            case EQUALITY:
+                return stringPath.eq(value);
+            case NEGATION:
+                return stringPath.ne(value);
+            case CONTAINS:
+                return stringPath.contains(value);
+            case STARTS_WITH:
+                return stringPath.startsWith(value);
+            default:
+                return null;
+        }
+    }
+
+    protected BooleanExpression getIntegerPredicate(final NumberPath<Integer> numberPath, final Integer value) {
+        switch (searchCriteria.getSearchOperation()) {
+            case EQUALITY:
+                return numberPath.eq(value);
+            case NEGATION:
+                return numberPath.ne(value);
+            case GREATER_THAN:
+                return numberPath.gt(value);
+            case GREATER_THAN_OR_EQUAL:
+                return numberPath.goe(value);
+            case LESS_THAN:
+                return numberPath.lt(value);
+            case LESS_THAN_OR_EQUAL:
+                return numberPath.loe(value);
+            default:
+                return null;
+        }
+    }
+
+    protected BooleanExpression getBigDecimalPredicate(final NumberPath<BigDecimal> numberPath, final BigDecimal value) {
+        switch (searchCriteria.getSearchOperation()) {
+            case EQUALITY:
+                return numberPath.eq(value);
+            case NEGATION:
+                return numberPath.ne(value);
+            case GREATER_THAN:
+                return numberPath.gt(value);
+            case GREATER_THAN_OR_EQUAL:
+                return numberPath.goe(value);
+            case LESS_THAN:
+                return numberPath.lt(value);
+            case LESS_THAN_OR_EQUAL:
+                return numberPath.loe(value);
+            default:
+                return null;
+        }
+    }
+
+    protected BooleanExpression getInstantPredicate(final DatePath<Instant> datePath, final Instant value) {
+        switch (searchCriteria.getSearchOperation()) {
+            case EQUALITY:
+                return datePath.eq(value);
+            case NEGATION:
+                return datePath.ne(value);
+            case GREATER_THAN:
+                return datePath.gt(value);
+            case GREATER_THAN_OR_EQUAL:
+                return datePath.goe(value);
+            case LESS_THAN:
+                return datePath.lt(value);
+            case LESS_THAN_OR_EQUAL:
+                return datePath.loe(value);
+            default:
+                return null;
+        }
+    }
+
+
+}
+
